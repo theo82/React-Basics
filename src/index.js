76df098d2fb4ceca47cb05b6581a811aadb28daf
theo82 <theoaristi53@gmail.com>
2021-05-08 16:34:21 +0300

    Simple List

--- src/index.js
+++ src/index.js
@@ -5,46 +5,27 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 //setup vars
-const firstBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/71xLmdLOQ0L._AC_UL200_SR200,200_.jpg',
-  title: 'Beyond Order: 12 More Rules for Life',
-  author: 'Peter B. Peterson',
-}
-
-const secondBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
-  title: 'A Promised Land',
-  author: 'Barack Obama',
-}
-
+const books = [
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71xLmdLOQ0L._AC_UL200_SR200,200_.jpg',
+    title: 'Beyond Order: 12 More Rules for Life',
+    author: 'Peter B. Peterson',
+  },
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
+    title: 'A Promised Land',
+    author: 'Barack Obama',
+  },
+]
+const names = ['john', 'peter', 'susan']
+const newName = names.map((name) => {
+  return <h1>{name}</h1>
+})
+console.log(newName)
 function BookList() {
-  return (
-    <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  )
+  return <section className='booklist'>{newName}</section>
 }
 
 // const Greetings = () => {
@@ -58,7 +39,6 @@ const Book = (props) => {
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   )
 }
