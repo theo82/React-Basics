3805db7e7f5ed9564bbc2d674dcdab58afea4f6d
theo82 <theoaristi53@gmail.com>
2021-05-09 07:47:09 +0300

    Event Basics

--- src/index.js
+++ src/index.js
@@ -49,15 +49,34 @@ function BookList() {
 // const Greetings = () => {
 //   return React.createElement('h1', 'hello word, I am Theo')
 // }
-const Book = (props) => {
+const Book = ({ img, title, author }) => {
   // const { img, title, author } = props.book
-  const { img, title, author } = props
-  console.log(props)
+  // const { img, title, author } = props
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert('Hello World')
+  }
+  const complexExample = (author) => {
+    console.log(author)
+  }
+
   return (
-    <article className='book'>
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(title)
+      }}
+    >
       <img src={img} alt='' />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        Reference Example
+      </button>
+      <button type='buttoon' onClick={() => complexExample(author)}>
+        More Complex Example
+      </button>
     </article>
   )
 }
