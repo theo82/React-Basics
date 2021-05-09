13e34cb3c2624aa79981876e0cb870e4472320a6
theo82 <theoaristi53@gmail.com>
2021-05-09 06:41:08 +0300

    Key Prop and Spread Operator

--- src/index.js
+++ src/index.js
@@ -7,16 +7,25 @@ import './index.css'
 //setup vars
 const books = [
   {
+    id: 1,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/71xLmdLOQ0L._AC_UL200_SR200,200_.jpg',
     title: 'Beyond Order: 12 More Rules for Life',
     author: 'Peter B. Peterson',
   },
   {
+    id: 2,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
     title: 'A Promised Land',
     author: 'Barack Obama',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81YzHKeWq7L._AC_UL200_SR200,200_.jpg',
+    title: 'The Midnight Library: A Novel',
+    author: 'Matt Haig',
   },
 ]
 
@@ -29,7 +38,8 @@ function BookList() {
           //   <h3>{title}</h3>
           //   <h6>{author}</h6>
           // </div>
-          <Book book={book}></Book>
+          //
+          <Book key={book.id} {...book} />
         )
       })}
     </section>
@@ -40,7 +50,8 @@ function BookList() {
 //   return React.createElement('h1', 'hello word, I am Theo')
 // }
 const Book = (props) => {
-  const { img, title, author } = props.book
+  // const { img, title, author } = props.book
+  const { img, title, author } = props
   console.log(props)
   return (
     <article className='book'>
